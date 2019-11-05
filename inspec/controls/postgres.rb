01ba892e26c861d91e8e9b3f5d1167af12d7427e
Mitchell, David <david.mitchell@gft.com>
2019-11-05 11:37:16 +0000

    commit

--- inspec/controls/postgres.rb
+++ inspec/controls/postgres.rb
@@ -1,6 +1,6 @@
 control "postgres" do                                # A unique ID for this control.                     # A human-readable title
   describe azurerm_postgresql_server(resource_group: 'postgresql-database-deutsche-bank', server_name: 'postgresql-database-deutsche-bank') do
       it { should exist }
-      its('name')   { should be 'mysqldatabse' }
+      its('name')   { should be 'postgresql-database-deutsche-bank' }
     end
 end
