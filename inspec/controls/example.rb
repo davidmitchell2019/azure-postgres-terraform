eda6b02ec34ef65d17f61eb00755153488c59e29
Mitchell, David <david.mitchell@gft.com>
2019-11-05 11:05:18 +0000

    test

--- inspec/controls/example.rb
+++ inspec/controls/example.rb
@@ -1,5 +1,5 @@
 control "test" do                                # A unique ID for this control.                     # A human-readable title
-  describe azurerm_postgresql_server(resource_group: 'inspec-resource-group-9', server_name: 'example_server') do
+  describe azurerm_postgresql_server(resource_group: 'postgresql-database-deutsche-bank', server_name: 'postgresql-database-deutsche-bank') do
       it { should exist }
     end
 end
