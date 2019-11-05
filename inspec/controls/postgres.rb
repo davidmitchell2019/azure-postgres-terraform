fc61b62c5cbde104921259e1f3a00e3219a7caa8
Mitchell, David <david.mitchell@gft.com>
2019-11-05 11:32:53 +0000

    adding test

--- inspec/controls/postgres.rb
+++ inspec/controls/postgres.rb
@@ -1,6 +1,6 @@
 control "postgres" do                                # A unique ID for this control.                     # A human-readable title
   describe azurerm_postgresql_server(resource_group: 'postgresql-database-deutsche-bank', server_name: 'postgresql-database-deutsche-bank') do
       it { should exist }
-      its('name')  { should include 'mysqldatabase' }
+      its('name')   { should be 'mysqldatabse' }
     end
 end
