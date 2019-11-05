aec8da4087c7051a55aefc3ae7ca9209fd4d0c66
Mitchell, David <david.mitchell@gft.com>
2019-11-05 19:47:32 +0000

    adding inspec inputs

--- inspec/controls/postgres.rb
+++ inspec/controls/postgres.rb
@@ -1,5 +1,5 @@
 control "postgres" do                                # A unique ID for this control.                     # A human-readable title
-  describe azurerm_postgresql_server(resource_group: 'postgresql-database-deutsche-bank', server_name: 'postgresql-database-deutsche-bank') do
+  describe azurerm_postgresql_server(resource_group: input('resource_group'), server_name: input('server_name')) do
       it { should exist }
     end
 end
