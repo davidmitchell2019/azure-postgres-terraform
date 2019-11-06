7b68ca1d04a3a341066d1b6f4ffb575a443fc2eb
Mitchell, David <david.mitchell@gft.com>
2019-11-06 10:15:08 +0000

    adding storage account

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 terraform {
   backend "azurerm" {
-    storage_account_name  = "AksTerraform-RG"
+    storage_account_name  = "dbsaccount2"
     container_name        = "tfstate-postgres"
     key                   = "terraform.tfstate"
     resource_group_name  = "postgresql-database-deutsche-bank"
