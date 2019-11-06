7298d9b937fe3ab8eda50d372f7d7ac477fbb348
Mitchell, David <david.mitchell@gft.com>
2019-11-06 09:55:47 +0000

    adding backend

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 provider "azurerm" {
   version = "=1.36.0"
 }
+terraform {
+  backend "azurerm" {
+    storage_account_name  = "dbsaccount"
+    container_name        = "tfstatepostgres"
+    key                   = "terraform.tfstate"
+  }
+}
