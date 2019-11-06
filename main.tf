b33497c58250794406b7793bf9942be4f3988ca7
Mitchell, David <david.mitchell@gft.com>
2019-11-06 13:42:57 +0000

    test

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 provider "azurerm" {
   version = "=1.36.0"
 }
+terraform {
+  backend "azurerm" {
+    storage_account_name  = "${var.storage_account_name}"
+    container_name        = "${var.storage_container_name}"
+    key                   = "terraform.tfstate"
+    resource_group_name  = "${var.resource_group_name}"
+  }
+}
 data "terraform_remote_state" "state" {
   backend = "azurerm"
   config = {
