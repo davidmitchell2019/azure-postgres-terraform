e652e93a6bf081a346649db048f631bb66f3d51c
Mitchell, David <david.mitchell@gft.com>
2019-11-06 10:11:37 +0000

    stuff

--- main.tf
+++ main.tf
@@ -1,24 +1,10 @@
 provider "azurerm" {
   version = "=1.36.0"
 }
-resource "azurerm_storage_account" "dbsaccount" {
-  name                     = "dbsaccount"
-  resource_group_name      = "postgresql-database-deutsche-bank"
-  location                 = "UK South"
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-}
-resource "azure_storage_blob" "tfstate" {
-  name                   = "tfstate"
-  storage_service_name   = "tfstate-store"
-  storage_container_name = "terraform-storage-container"
-  type                   = "PageBlob"
-  size                   = 1024
-}
 terraform {
   backend "azurerm" {
-    storage_account_name  = "tfstate"
-    container_name        = "terraform-storage-container"
+    storage_account_name  = "AksTerraform-RG"
+    container_name        = "tfstate-postgres"
     key                   = "terraform.tfstate"
     resource_group_name  = "postgresql-database-deutsche-bank"
   }
