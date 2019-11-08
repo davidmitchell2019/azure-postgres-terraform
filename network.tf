ce692a3891af4aa16668dc75cdb6039d8b182647
Mitchell, David <david.mitchell@gft.com>
2019-11-08 14:24:52 +0000

    adding security group assiciation

--- network.tf
+++ network.tf
@@ -30,29 +30,35 @@ resource "azurerm_network_security_group" "vnet-security" {
   location            = "UKSouth"
   resource_group_name = "${var.resource_group_name}"
 }
-resource "azurerm_network_security_rule" "egress-rule" {
-  name                        = "allow-egress"
-  priority                    = 100
-  direction                   = "Outbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = "${var.resource_group_name}"
-  network_security_group_name = "${azurerm_network_security_group.vnet-security.name}"
+resource "azurerm_network_security_group" "test" {
+  name                = "example-nsg"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+
+  security_rule {
+    name                        = "allow-ingress"
+    priority                    = 100
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "*"
+    source_address_prefix       = "79.66.41.57/32"
+    destination_address_prefix  = "*"
+  }
+  security_rule {
+    name                        = "allow-egress"
+    priority                    = 100
+    direction                   = "Outbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "*"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+  }
 }
-resource "azurerm_network_security_rule" "ingress-rule" {
-  name                        = "allow-ingress"
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "79.66.41.57/32"
-  destination_address_prefix  = "*"
-  resource_group_name         = "${var.resource_group_name}"
-  network_security_group_name = "${azurerm_network_security_group.vnet-security.name}"
+resource "azurerm_subnet_network_security_group_association" "apply-sg-to-vnet" {
+  subnet_id                 = "${azurerm_subnet.subnet.id}"
+  network_security_group_id = "${azurerm_network_security_group.test.id}"
 }
