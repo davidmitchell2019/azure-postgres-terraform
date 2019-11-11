9d4337da5c72a4e211d60898da9853e8efee9bb6
Mitchell, David <david.mitchell@gft.com>
2019-11-11 15:46:57 +0000

    adding depends on

--- modules/postgres/network.tf
+++ modules/postgres/network.tf
@@ -50,5 +50,5 @@ resource "azurerm_network_security_group" "sg" {
 resource "azurerm_subnet_network_security_group_association" "apply-sg-to-vnet" {
   subnet_id                 = "${azurerm_subnet.subnet.id}"
   network_security_group_id = "${azurerm_network_security_group.sg.id}"
-  depends_on = [azurerm_network_security_group.sg, azurerm_virtual_network.vnet]
+  depends_on = [azurerm_network_security_group.sg, azurerm_virtual_network.vnet, azurerm_subnet.subnet]
 }
